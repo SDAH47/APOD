2efcc9f183ea2c505efc56f38678a40126e6dcdc
SDAH47 <58889389+SDAH47@users.noreply.github.com>
2020-01-16 02:25:34 +0600

    Add files via upload

--- templates/app.js
+++ templates/app.js
@@ -1,15 +1,64 @@
-const nasa = new Nasa;
-const titleUI = document.querySelector('#title');
-const copyrightUI = document.querySelector('#copyright');
-const dateUI = document.querySelector('#date');
-const explanationUI = document.querySelector('#explanation');
-const imgUI = document.querySelector('#nasaImg');
-let apiData="";
-nasa.getData().then(data =>{
-    apiData=data.getData;
-    titleUI.innerText = title =apiData.title;
-    copyrightUI.innerText=apiData.copyright;
-    dateUI.innerText = apiData.date;
-    explanationUI.innerText = content = apiData.explanation;
-    imgUI.src = url = apiData.url;
-});
+// Setting the date to now date.
+var dateContent = document.querySelector( 'input.date' ),
+date = new Date(),
+month = date.getMonth() + 1;
+
+dateContent.value = date.getFullYear() + '-' + ( month < 10 ? '0' + month : month ) + '-' + ( date.getDate() - 1 );
+
+function load() {
+	const nasa = new Nasa;
+	const titleUI = document.querySelector('#title');
+	const copyrightUI = document.querySelector('#copyright');
+	const dateUI = document.querySelector('#date');
+	const explanationUI = document.querySelector('#explanation');
+	const imgUI = document.querySelector('#nasaImg');
+	
+	var val = dateContent.value.split( '-' ).map( e => Number( e ) );
+	
+	var c_date = val[ 2 ] < date.getDate() && val[ 1 ] <= month && ( val[ 0 ] <= date.getFullYear() && val[ 0 ] >= 1995 );
+	
+	if( c_date ) {
+		const dataJSON = nasa.getData( dateContent.value )
+		
+		dataJSON.then(data => {data
+			titleUI.innerText = title =data.title;
+			copyrightUI.innerText=data.copyright;
+			dateUI.innerText = data.date;
+			explanationUI.innerText = content = data.explanation;
+			imgUI.src = url = data.url;
+		});
+		
+		document.querySelector( 'div#info' ).style.display = "block";
+	} else { 
+		document.querySelector( 'div.error' ).innerHTML = '404 Error! The date is in future!';
+		document.querySelector( 'div#info' ).style.display = "none";
+	}
+}
+
+function downloadPDF() {
+	var doc = new jsPDF();
+    doc.setFont("courier");
+    var elementHandler = {
+      
+    };
+    var source = document.createElement( 'div' );
+    
+    var title = document.querySelector( '#title' ),
+    subText = document.querySelector( '#subText' ),
+    p = document.querySelector( 'p' );
+    
+    var src = document.querySelector( 'div.pdf' );
+    console.log(src)
+    
+    source.appendChild( title.cloneNode( true ) );
+    source.appendChild( subText.cloneNode( true ) );
+    source.appendChild( p.cloneNode( true ) );
+    doc.fromHTML(
+      source,
+      15,
+      0.5,
+      {
+        'width': 180,'elementHandlers': elementHandler
+      });
+        doc.save('abc.pdf');
+}
--- templates/nasa.js
+++ templates/nasa.js
@@ -3,11 +3,11 @@ class Nasa{
         this.api = 'GdWau7tPdPNkucMLX5xgTIDTL9cELo21FGPAdQmP';
     }
 
-    async getData(){
-        const getResponse = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${this.api}`);
-
-        const getData = await getResponse.json();
-
-        return {getData}
+    async getData( date ){
+	    console.log(date);
+        const getResponse = await fetch(`https://api.nasa.gov/planetary/apod?start_date=${date}&end_date=${date}&api_key=GdWau7tPdPNkucMLX5xgTIDTL9cELo21FGPAdQmP`);
+	   var getData = await getResponse.json();
+	   
+	   return getData[ 0 ];
     }
 }
